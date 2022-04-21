6197e3f819c1c1185cf8d495aa288e9e7b2869de
y <y@DESKTOP-EDBR2CG>
2022-04-21 10:02:44 +0900

    2022-04-21 Ex08수정

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -4,21 +4,21 @@ public class Ex08 {
 
 	public static void main(String[] args) {
 				
-		int[] lotto1 = new int[6];
-		int[] lotto2 = new int[6];
+		int[] lotto = new int[6];
 		
-		if(lotto1.length == lotto2.length) {
-			//두 배열의 갯수가 같니?
-			for(int i=0; i<lotto1.length; i++) {
-			lotto1[i] = (int)(Math.random()*45)+1;
-				
-			if(lotto1[i] != lotto2[i]) {
-			//각방의 데이터가 다르니?
-			//다르면,
-			System.out.print(lotto1[i] + "\t");
-				}	
+		for(int i=0; i<lotto.length; i++) {
+			lotto[i] = (int)(Math.random()*45)+1;
+			
+			System.out.print(lotto[i]+"\t");
+			
+			for(int j=0; j<i; j++) {
+				if(lotto[i]!=lotto[j]) {
+					break;
+				}
 			}
 		}
+		
+		
 	
 	
 	}
