80d15b015a19ff5e9adf607bd92d7e23ca53deec
GyeongAeKim <kgae128@gmail.com>
2022-04-21 11:20:53 +0900

    2022-04-21 practice04 ex08

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -9,14 +9,28 @@ public class Ex08 {
 		for(int i=0; i<lotto.length; i++) {
 			lotto[i] = (int)(Math.random()*45)+1;
 			
-			System.out.print(lotto[i]+"\t");
+			boolean isDuplicated = false;
+			
+			//System.out.print(lotto[i]+"\t");
 			
 			for(int j=0; j<i; j++) {
-				if(lotto[i]!=lotto[j]) {
-					break;
+				if(lotto[i]==lotto[j]) {
+					isDuplicated = true;
+					System.out.println(lotto[i]+" 중복됨");
 				}
 			}
+		
+			if(isDuplicated == true) {
+				i--;
+				continue; //중복된 수 제외, 조건식 다시 증감,.
+			}
+		
 		}
+		//재출력
+		for(int i=0; i<lotto.length; i++) {
+			System.out.print(lotto[i]+" ");
+		}
+
 		
 		
 	
